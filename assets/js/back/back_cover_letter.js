80488c8ed771c99dbeaa1e6e58181e4df5af87fa
Mathias Daverède <daverede.mathias@gmail.com>
2025-10-12 19:14:42 +0200

    chore: add note template (checker svg comme l'icone Stymfony) [refs #3]

--- assets/js/back/back_cover_letter.js
+++ assets/js/back/back_cover_letter.js
@@ -1,1 +1,6 @@
 import '../../styles/back/back_cover_letter.scss';
+import { CopyButton } from './copy_button.js';
+
+document.querySelectorAll('.btn-copy').forEach(button => {
+    new CopyButton(button);
+});
