ce27a719cf0959ad3cabb9096595655d5ff05858
Mathias Daverède <daverede.mathias@gmail.com>
2025-10-13 14:48:08 +0200

    fix: Rename generation pdf command [refs #5]

--- src/Command/GeneratePdfCommand.php
+++ src/Command/GeneratePdfCommand.php
@@ -8,17 +8,29 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Knp\Snappy\Pdf;
 
-#[AsCommand(name: 'app:generate-cover-letter')]
-class GenerateCoverLetterCommand extends Command
+#[AsCommand(
+    name: 'app:generate-pdf',
+    description: 'Generate a Pdf to test in CI'
+)]
+class GeneratePdfCommand extends Command
 {
     private $snappy;
 
+    /**
+     * @param Pdf $snappy 
+     */
     public function __construct(Pdf $snappy)
     {
         $this->snappy = $snappy;
         parent::__construct();
     }
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return int
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $html = '<h1>Test Cover Letter</h1><p>Hello, world!</p>';
