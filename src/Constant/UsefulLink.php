8eb8af0ce6559806f6579e2bfd2319698973b8d3
Mathias Daverède <daverede.mathias@gmail.com>
2025-10-14 18:35:07 +0200

    affichage la de version

--- src/Constant/UsefulLink.php
+++ src/Constant/UsefulLink.php
@@ -4,6 +4,10 @@ namespace App\Constant;
 
 class UsefulLink
 {
+    public const GITHUB_REPO = 'https://github.com/MathiasDaverede/job-search';
+    public const GITHUB_CHANGELOG = 'https://github.com/MathiasDaverede/job-search/blob/main/CHANGELOG.md';
+    public const CODINGAME_PROFILE = 'https://www.codingame.com/profile/8ee16a3f95abdb6ba27dd653534e016e4923462';
+
     public const BACK = [
         'Symfony 7.3' => 'https://symfony.com/doc/7.3',
         'PHP' => 'https://www.php.net/manual/fr/',
