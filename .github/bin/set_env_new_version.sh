2055fee8195321e73b8146fb0b646423100aff73
Mathias Daverède <daverede.mathias@gmail.com>
2025-10-17 17:11:41 +0200

    restructuration CI

--- .github/bin/set_env_new_version.sh
+++ .github/bin/set_env_new_version.sh
@@ -5,7 +5,5 @@ set -e  # Exit on any error
 pr_branch=$1 # ${{ github.event.pull_request.head.ref }}
 
 version=$(echo "$pr_branch" | sed -E 's/^(release|hotfix)\/([0-9]+\.[0-9]+\.[0-9]+)$/\2/')
-new_version="v$version"
 
-echo "NEW_VERSION=$new_version" >> "$GITHUB_ENV" # ${{ env.NEW_VERSION }}
-echo "$new_version" > VERSION.md
+echo "NEW_VERSION=v$version" >> "$GITHUB_ENV" # ${{ env.NEW_VERSION }}
