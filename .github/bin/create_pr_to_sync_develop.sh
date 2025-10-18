5c3af82d1b24748d66f2385282f14e8d63858c8f
Mathias Daverède <daverede.mathias@gmail.com>
2025-10-18 09:39:27 +0200

    aaa

--- .github/bin/create_pr_to_sync_develop.sh
+++ .github/bin/create_pr_to_sync_develop.sh
@@ -15,9 +15,9 @@ pr_body+="4. Update this PR to use 'sync-main-$new_version-to-develop' as the so
 pr_body+="5. Verify tests pass before merging."
 
 echo "pr_title: $pr_title"
-echo "pr_body: $pr_body"
+echo -e "pr_body:\n$pr_body"
 
-pr_url=$(gh pr create --base develop --head main --title "$pr_title" --body "$pr_body" --label sync)
+pr_url=$(gh pr create --base develop --head main --title "$pr_title" --body "$pr_body" --label synchronization)
 
 echo "pr_url: $pr_url"
 
@@ -25,13 +25,21 @@ pr_number=$(echo "$pr_url" | grep -oE '[0-9]+$')
 
 echo "pr_number=$pr_number" >> $GITHUB_OUTPUT
 
-# Verify if thre are conflicts in PR
+# Verify if there are conflicts in PR
 conflict_status=$(gh api repos/{owner}/{repo}/pulls/$pr_number | jq -r .mergeable)
 
 echo "conflict_status=$conflict_status" >> $GITHUB_OUTPUT
 
 if [ "$conflict_status" = "false" ]; then
-    gh pr comment $pr_number --body "This PR has merge conflicts. Since 'main' is protected, please resolve them manually:\n1. Create a branch 'sync-develop-${{ env.NEW_VERSION }}' from main.\n2. Merge develop into this branch and resolve conflicts (e.g., via SourceTree).\n3. Push the branch to origin.\n4. Update this PR to use 'sync-develop-${{ env.NEW_VERSION }}' as the source branch.\n5. Verify tests pass."
+    comment="This PR has merge conflicts.\n"
+    comment+="Since 'main' is protected, please resolve them manually:\n"
+    comment+="1. Create a branch 'sync-main-$new_version-to-develop' from develop.\n"
+    comment+="2. Merge main into this branch and resolve conflicts.\n"
+    comment+="3. Push the branch to origin.\n"
+    comment+="4. Update this PR to use 'sync-main-$new_version-to-develop' as the source branch.\n"
+    comment+="5. Verify tests pass."
+    
+    gh pr comment $pr_number --body $comment
 else
     echo "No merge conflicts detected in PR #$pr_number"
 fi
