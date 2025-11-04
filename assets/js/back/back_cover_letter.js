c0bc78ca87ae7e13746da06c5e67bfe419696780
Mathias Daverède <daverede.mathias@gmail.com>
2025-11-04 01:34:06 +0100

    chore: create front controller to generate cover letters PDF (ref #4)

--- assets/js/back/back_cover_letter.js
+++ assets/js/back/back_cover_letter.js
@@ -1,5 +1,6 @@
 import '../../styles/back/back_cover_letter.scss';
 
+import { Tooltip } from 'bootstrap';
 import { CopyButton } from './copy_button.js';
 
 /**
@@ -9,6 +10,16 @@ import { CopyButton } from './copy_button.js';
  * as Turbo replaces the <body> content without triggering a full page reload or DOMContentLoaded.
  */
 document.addEventListener('turbo:load', () => {
+    const tooltips = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    [...tooltips].map(tooltipElement => new Tooltip(tooltipElement));
+    
+    document.getElementById('btn-generate-pdf').addEventListener('click', (event) => {
+        const button = event.currentTarget;
+        const pdfUrl = button.dataset.pdfUrl;
+
+        window.open(pdfUrl, '_blank');
+    });
+
     document.querySelectorAll('.btn-copy').forEach(button => {
         new CopyButton(button);
     });
